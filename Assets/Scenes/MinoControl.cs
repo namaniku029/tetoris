f9a5a108e56274a6517bdcc3e6ff01d78a7b10ca
namaniku029 <namaniku555@gmail.com>
2019-11-28 17:47:09 +0900

    first commit

---
+++ Assets/Scenes/MinoControl.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinoControl : MonoBehaviour
+{
+    GameObject[] minos;
+    float[] minoX = new float[260];
+    float[] minoY = new float[260];
+    float span = 1.0f;
+    float delta = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.minos = GameObject.FindGameObjectsWithTag("mino");
+        int i = 0;
+        foreach (GameObject mino in this.minos)
+        {
+            //丸め誤差解消
+            this.minoX[i] = Mathf.RoundToInt(mino.transform.position.x * 10.0f) / 10.0f;
+            this.minoY[i] = Mathf.RoundToInt(mino.transform.position.y * 10.0f) / 10.0f;
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int a = 0;
+        //ミノを自然落下させる
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            transform.Translate(0, -1, 0, Space.World);
+
+        }
+        //ミノの移動操作
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(-1f, 0f, 0f, Space.World);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.Translate(1f, 0f, 0f, Space.World);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.transform.Translate(0f, -1f, 0f, Space.World);
+        }
+        //回転
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (transform.position.x <=2.5 || transform.position.x >=-2.5)
+            {
+                transform.Rotate(0, 0, 90.0f);
+            }
+            
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+            if (transform.position.x <= 2.5 || transform.position.x >= -2.5)
+            {
+                transform.Rotate(0, 0, -90.0f);
+            }
+
+        a = 0;
+        //親オブジェクトの下にミノがあった場合そこで止まる
+        foreach (GameObject mino in this.minos) {
+            if (transform.position.x == this.minoX[a] && transform.position.y == this.minoY[a] + 1.0f){
+                gameObject.transform.DetachChildren();    //親子関係の解除
+                Destroy(gameObject);     //minoセットオブジェクト（親）を削除
+                return;
+            }
+            
+            foreach (Transform child in transform){
+                if (child.gameObject.transform.position.x == this.minoX[a] && child.gameObject.transform.position.x == this.minoY[a]) {
+                    gameObject.transform.DetachChildren();    //親子関係の解除
+                    Destroy(gameObject);     //minoセットオブジェクト（親）を削除
+                    return;
+                }
+            }
+
+            a++;
+        }
+        //地面についたとき親解除
+        if (transform.position.y == 0.5f)
+        {
+            gameObject.transform.DetachChildren();
+            Destroy(gameObject);
+        }
+
+
+    }
+}
+    
+
---
+++ Assets/Scenes/MinoControl2.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinoControl2 : MonoBehaviour
+{
+    readonly Block[] minos = new Block[]{
+        new Block(
+             new Vector2[,] { 
+                {new Vector2(-1,0),new Vector2(0,0),new Vector2(1,0),new Vector2(2,0) },
+                {new Vector2(0,-1),new Vector2(0,0),new Vector2(0,1),new Vector2(0,2) }
+              }
+        ),
+        new Block(
+             new Vector2[,] {
+                {new Vector2(0,1),new Vector2(0,0),new Vector2(0,-1),new Vector2(-1,-1)},
+                {new Vector2(1,0),new Vector2(0,0),new Vector2(-1,0),new Vector2(-1,1) },
+                {new Vector2(0,-1),new Vector2(0,0),new Vector2(0,1),new Vector2(1,1) },
+                {new Vector2(-1,0),new Vector2(0,0),new Vector2(1,0),new Vector2(1,-1) }
+              }
+        ),
+
+
+    };
+    //ステージの配列化
+    float[,] stage = new float[12, 25];
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ステージの壁部分に１(壁)を代入
+        for (int i = 0;i<12;i++)
+            for (int j =0;j<25;j++ )
+                if (i == 0 || i==11 ||j ==0)
+                    stage[i, j] = 1;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0;i<4000;i++) {
+            if (i /2 == 0 ) {
+                minos[0].MinoStartPos();
+            }
+        }
+    }
+}
+
+class Block : MinoControl2
+{
+    GameObject mino;
+    public Vector2[,] Mino;
+    public Block(Vector2[,] Mino)
+    {
+        this.Mino = Mino;
+    }
+
+    public void MinoStartPos()
+    {
+        Vector2 StartPos = new Vector2(6.5f, 22.5f);
+        transform.Translate(StartPos.x,StartPos.y,0,Space.World);
+    }
+}
